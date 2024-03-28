79790465f7060b01e06e008deb858e5d17173783
janak <dsandeshhh@gmail.com>
2024-03-28 19:15:41 +0545

    .

--- assets/js/form4.js
+++ assets/js/form4.js
@@ -1,7 +1,7 @@
 function submitForm() {
     // Perform form submission
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://docs.google.com/forms/d/e/1FAIpQLSdoD2-c2RrVQofvuxLla2jipJTCZF7FY1R6e17fhTHuJQ-ZBA/formResponse", true);
+    xhr.open("POST", "https://docs.google.com/forms/d/e/1FAIpQLSfrCvdwtKB5jSJOVyr0ZG2RBnG5Ei_RXmEcG7BHdfvczHoe8g/formResponse", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     var formData = new FormData(document.getElementById("investorForm"));
     xhr.send(new URLSearchParams(formData).toString());

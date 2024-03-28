c7c017d0cd191b64a0983013c3e9d908a6fd53c5
janak <dsandeshhh@gmail.com>
2024-03-28 19:38:52 +0545

    .

--- assets/js/form4.js
+++ assets/js/form4.js
@@ -1,13 +1,15 @@
-function submitForm() {
-    // Perform form submission
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://docs.google.com/forms/d/e/1FAIpQLSfrCvdwtKB5jSJOVyr0ZG2RBnG5Ei_RXmEcG7BHdfvczHoe8g/formResponse", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    var formData = new FormData(document.getElementById("investorForm"));
-    xhr.send(new URLSearchParams(formData).toString());
 
-    // Display thank-you message
-    document.getElementById("investorForm").style.display = "none";
-    document.getElementById("thankYouMessage").style.display = "block";
-    return false; // Prevent default form submission
-}
+    function submitForm() {
+        // Perform form submission
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "https://docs.google.com/forms/d/e/1FAIpQLSfrCvdwtKB5jSJOVyr0ZG2RBnG5Ei_RXmEcG7BHdfvczHoe8g/formResponse", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        var formData = new FormData(document.getElementById("investorForm"));
+        xhr.send(new URLSearchParams(formData).toString());
+
+        // Display thank-you message
+        document.getElementById("investorForm").style.display = "none";
+        document.getElementById("thankYouMessage").style.display = "block";
+
+        return false; // Prevent default form submission
+    }

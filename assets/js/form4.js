e1f487172f6603aa68677cd875655c4568ae0a85
janak <dsandeshhh@gmail.com>
2024-03-28 17:13:32 +0545

    .

---
+++ assets/js/form4.js
@@ -0,0 +1,13 @@
+function submitForm() {
+    // Perform form submission
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://docs.google.com/forms/d/e/1FAIpQLSdoD2-c2RrVQofvuxLla2jipJTCZF7FY1R6e17fhTHuJQ-ZBA/formResponse", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    var formData = new FormData(document.getElementById("investorForm"));
+    xhr.send(new URLSearchParams(formData).toString());
+
+    // Display thank-you message
+    document.getElementById("investorForm").style.display = "none";
+    document.getElementById("thankYouMessage").style.display = "block";
+    return false; // Prevent default form submission
+}
